5cd907c764820508fac93042695ced9ac44677d0
nguyenhung <nguyenhung2004200@gmial.com>
2024-12-04 22:13:22 +0700

    update lazy

--- main.js
+++ main.js
@@ -137,3 +137,21 @@ function renderComments() {
 renderComments();
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  const lazyImages = document.querySelectorAll("img.lazy");
+  
+  const imageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              const img = entry.target;
+              img.src = img.dataset.src;
+              img.classList.remove("lazy");
+              img.classList.add("loaded");
+              observer.unobserve(img);
+          }
+      });
+  });
+
+  lazyImages.forEach(img => imageObserver.observe(img));
+});
+
